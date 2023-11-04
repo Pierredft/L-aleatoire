e5361b1b7e1b1dc9da661e55126bfcd69db4ade0
pierre defauquet <pierredefauquet@gmail.com>
2023-11-04 22:10:00 +0100

    maj#2

---
+++ Accueil.js
@@ -0,0 +1,27 @@
+
+  // Fonction pour générer un nombre aléatoire entre min et max
+  function generateRandomNumber() {
+    var minInput = document.getElementById("min");
+    var maxInput = document.getElementById("max");
+    var resultatElement = document.getElementById("resultat");
+
+    // Obtenez les valeurs des champs "min" et "max"
+    var min = parseInt(minInput.value);
+    var max = parseInt(maxInput.value);
+
+    // Vérifiez que les valeurs sont des nombres valides
+    if (!isNaN(min) && !isNaN(max)) {
+      // Générez un nombre aléatoire entre min et max
+      var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      // Affichez le résultat dans l'élément "resultat"
+      resultatElement.textContent = randomNum;
+    } else {
+      // Affichez un message d'erreur si les valeurs ne sont pas valides
+      resultatElement.textContent = "Veuillez entrer des nombres valides pour min et max.";
+    }
+  }
+
+  // Écoutez le clic sur le bouton "Générer" et appelez la fonction generateRandomNumber()
+  var generateButton = document.querySelector('input[type="submit"]');
+  generateButton.addEventListener("click", generateRandomNumber);
